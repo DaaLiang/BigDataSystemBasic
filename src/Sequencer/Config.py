f4b88b8aa1c5c2097a52738e398de8c78796d899
yuchuan <hecex@outlook.com>
2018-12-19 20:25:33 +0800

    add comments for SequencerConfig

--- src/Sequencer/Config.py
+++ src/Sequencer/Config.py
@@ -14,12 +14,18 @@ class DealerConfig(Config):
 
 
 class SequencerConfig(Config):
+    # 排序时间间隔，应该和 broker 的收集时间间隔相等
+    INTERVAL = 1
 
+    # 监听 broker 的端口
     LISTEN_PORT = ("0.0.0.0", 6666)
+    # 最大监听连接数量
+    LISTEN_NUM = 5
+    # 缓冲区大小
     RECV_BUFF_SIZE = 4096
 
-    INTERVAL = 0.05
-    ELAPSE = 0.1
-
-    MULTICAST_ADD = ('192.168.0.1', 60003)
-    SENDER_ADD =('0.0.0.0', 6001)
+    # 组播的源地址
+    SENDER_ADD = ('192.168.0.101', 60001)
+    # SENDER_ADD = ('10.13.2.149', 60001)
+    # 组播的目的地址
+    MULTICAST_DST = (DealerConfig.MULTICAST_GROUP, DealerConfig.LISTEN_SOCKET[1])
